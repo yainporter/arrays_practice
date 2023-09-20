1f143898698917b4d72ca4db4eb87261f7bc52a4
Yain Porter <yainporter@gmail.com>
2023-09-19 20:05:28 -0400

    Will demonstrate understanding of index positions

--- arrays.rb
+++ arrays.rb
@@ -15,3 +15,14 @@ p numbers.push 55
 # The `unshift` method adds an element to the beginning of an array
 # and returns the new array
 p has_kids.unshift true
+
+# Demonstrate understanding of index positions
+
+# An index position of 0 for the array `names` will yield Yain
+p names[0]
+
+#An index position of 3 for the array `numbers` will yield 44
+p numbers[3]
+
+# The index position of arrays start off by counting by 1s with 0 
+# for the first element
